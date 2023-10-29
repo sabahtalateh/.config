41c05e2d34919be72f489d7e3a8bd6139ae7bfd2
sabahtalateh <sabahtalateh@gmail.com>
2023-10-29 14:25:42 +0300

    commit

--- wezterm/nvim.lua
+++ wezterm/nvim.lua
@@ -7,13 +7,10 @@ config.window_background_opacity = 1.0
 
 config.use_fancy_tab_bar = false
 
-config.window_frame.active_titlebar_bg = '#7f796d'
-config.window_frame.inactive_titlebar_bg = '#7f796d'
-
 config.colors = {
     tab_bar = {
         background = '#fdf8f4',
-        active_tab = { bg_color = '#fdf8f4', fg_color = '#494441', underline = 'Double', },
+        active_tab = { bg_color = '#fdf8f4', fg_color = '#494441', underline = 'None', },
         inactive_tab = { bg_color = '#d6c9c2', fg_color = '#494441' },
         inactive_tab_hover = { bg_color = '#fdf8f4', fg_color = '#494441' },
         new_tab = { bg_color = '#fdf8f4', fg_color = '#494441' },
